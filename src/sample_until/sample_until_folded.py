77acc03ebf1e0625c84ab9c2bafd7d0ee3ab5c70
Marvin Lücke <31667561+lueckem@users.noreply.github.com>
2025-06-27 15:27:53 +0200

    sample_until_folded (#4)

---
+++ src/sample_until/sample_until_folded.py
@@ -0,0 +1,129 @@
+import multiprocessing as mp
+from itertools import islice
+from multiprocessing.managers import Namespace
+from typing import Any, Callable, Iterable, Optional
+
+from .stopping_conditions import StoppingCondition, stop
+from .utils import check_fold_function, sanitize_inputs
+
+
+def sample_until_folded(
+    f: Callable,
+    fold_function: Callable,
+    fold_initial: Any,
+    f_args: Optional[Iterable] = None,
+    duration_seconds: Optional[float] = None,
+    num_samples: Optional[int] = None,
+    memory_percentage: Optional[float] = None,
+    num_workers: Optional[int] = None,
+) -> tuple[Any, int]:
+    """
+    Run `f` repeatedly until one of the given conditions is met and aggregate its outputs.
+
+    The function `f` should either accept no arguments, or exactly one argument that is generated for each sample via `f_args`.
+    If `f_args` is finite, running out of arguments is also a stopping condition.
+    The stopping conditions might not be respected exactly,
+    e.g., the elapsed time can be slightly longer than `duration_seconds`.
+
+    The outputs of `f` are folded together (accumulated) via the `fold_function` into `acc`,
+    i.e., `acc = fold_function(acc, f())` with initial value `acc = fold_initial`.
+    For example, to sum up all outputs of `f`, use `fold_function(acc, x) = acc + x`.
+
+    Args:
+        f: Function to sample.
+        fold_function: Function used for accumulating results.
+        fold_initial: Initial value for the accumulation.
+        f_args: Iterable that generates input arguments for `f`.
+        duration_seconds: Stop after time elapsed.
+        num_samples: Stop after number of samples acquired.
+        memory_percentage: Stop after system memory exceeds percentage, e.g., `0.8`.
+        num_workers: Number of processes (defaults to 1). Pass `-1` for number of cpus.
+
+    Returns:
+        Accumulated result `acc` and number of iterations.
+    """
+    f1, f_args, num_workers, stopping_conditions = sanitize_inputs(
+        f, f_args, duration_seconds, num_samples, memory_percentage, num_workers
+    )
+
+    # check if it accepts exactly 2 arguments
+    check_fold_function(fold_function)
+
+    # no multiprocessing
+    if num_workers == 1:
+        return _sample_until_folded(
+            f1, fold_function, fold_initial, f_args, stopping_conditions
+        )
+
+    # multiprocessing
+    manager = mp.Manager()
+    namespace = manager.Namespace()
+    namespace.acc = fold_initial
+    namespace.num_iter = 0
+    lock = mp.Lock()
+
+    processes = [
+        mp.Process(
+            target=_worker,
+            args=(
+                f1,
+                fold_function,
+                islice(f_args, i, None, num_workers),
+                stopping_conditions,
+                namespace,
+                lock,
+            ),
+        )
+        for i in range(num_workers)
+    ]
+
+    for p in processes:
+        p.start()
+
+    for p in processes:
+        p.join()
+
+    return namespace.acc, namespace.num_iter
+
+
+def _sample_until_folded(
+    f: Callable,
+    fold_function: Callable,
+    fold_initial: Any,
+    f_args: Iterable,
+    stopping_conditions: list[StoppingCondition],
+):
+    acc = fold_initial
+    i = 0
+    for a in f_args:
+        acc = fold_function(acc, f(a))
+        i += 1
+
+        if stop(stopping_conditions, i):
+            return acc, i
+
+    print("Stopped because all f_args were used.")
+    return acc, i
+
+
+def _worker(
+    f: Callable,
+    fold_function: Callable,
+    f_args: Iterable,
+    stopping_conditions: list[StoppingCondition],
+    namespace: Namespace,
+    lock,
+):
+    i = 0
+    for a in f_args:
+        x = f(a)
+        with lock:
+            namespace.acc = fold_function(namespace.acc, x)
+            namespace.num_iter += 1
+        i += 1
+
+        if stop(stopping_conditions, i):
+            return
+
+    print("Stopped because all f_args were used.")
+    return
---
+++ tests/test_fold.py
@@ -0,0 +1,46 @@
+import pytest
+
+from sample_until import sample_until_folded
+
+
+def sample(x):
+    return x
+
+
+@pytest.fixture
+def f_args():
+    return (i for i in range(100))
+
+
+def fold_sum(acc, x):
+    return acc + x
+
+
+def test_fold(f_args):
+    out = sample_until_folded(sample, fold_sum, 10, f_args=f_args)
+    assert out == (100 * 99 / 2 + 10, 100)
+
+
+def test_fold_stop_num_samples(f_args):
+    out = sample_until_folded(sample, fold_sum, 10, f_args=f_args, num_samples=50)
+    assert out == (50 * 49 / 2 + 10, 50)
+
+
+def test_fold_multiprocessing(f_args):
+    out = sample_until_folded(sample, fold_sum, 10, f_args=f_args, num_workers=4)
+    assert out == (100 * 99 / 2 + 10, 100)
+
+
+def test_fold_multiprocessing_stop_num_samples(f_args):
+    out = sample_until_folded(
+        sample, fold_sum, 10, f_args=f_args, num_samples=40, num_workers=4
+    )
+    assert out == (40 * 39 / 2 + 10, 40)
+
+
+def test_fold_invalid_fold_function(f_args):
+    def invalid_fold(acc):
+        return acc
+
+    with pytest.raises(ValueError):
+        sample_until_folded(sample, invalid_fold, 0, f_args=f_args)
